eb039f7963da90e48cdcebde3ab69d6800429a0c
localadmin <localadmi@server.telmex.com>
2013-02-01 16:59:08 -0600

    First commit

---
+++ include/db.php
@@ -0,0 +1,52 @@
+<?php
+
+// mysql hostname
+$db_host = "192.168.10.240";
+
+// database name
+$db_name = "startup";
+
+// database user name
+$db_user = "root";
+
+// database password
+$db_pass = "moises";
+
+// admin username
+$admin_user = "admin";
+
+// admin password
+$admin_pass = "admin";
+
+// HTML that goes just before </head>
+$head_html = "";
+
+// attribution (must leave link intact, per our license)
+$attribution = "
+  <span>
+    Based on <a href='http://www.represent.la' target='_blank'>RepresentLA</a>
+  </span>
+";
+
+// show events on the map? if set to "true", an event
+// category will appear in the marker list, and you can
+// run events_get.php in your browser (or a chron) to populate
+// it with data from eventbrite.
+$show_events = true;
+
+    // put your eventbrite api key here
+    $eb_app_key = "135450031447556006295";
+
+    // search eventbrite for these keywords
+    // add %20OR%20 between keywords for an "OR" search
+    // example: startup%20OR%20demo+day
+    $eb_keywords = "startup";
+
+    // specify city to search in and around
+    // example: Santa+Monica
+    $eb_city = "Santa+Monica";
+
+    // specify search radius (in miles)
+    $eb_within_radius = 100;
+
+?>
